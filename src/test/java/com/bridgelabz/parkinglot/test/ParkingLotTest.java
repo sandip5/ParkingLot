e30f88645ceb164464a3e51402bff151926d9350
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-17 22:27:24 +0530

    ADDED- Welcome Test Case

---
+++ src/test/java/com/bridgelabz/parkinglot/test/ParkingLotTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.parkinglot.test;
+
+import org.junit.Test;
+
+public class ParkingLotTest {
+    @Test
+    public void welcomeTestCase() {
+        System.out.println("Welcome To Parking Lot Problem");
+    }
+}
