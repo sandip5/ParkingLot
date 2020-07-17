c0fea8b2699a48dcb45f5b8b87fec3b4e46a5000
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-18 02:24:44 +0530

    ADDED- Test Case For Checking Lot Full Or Not

--- src/main/java/com/bridgelabz/parkinglot/service/ParkingLot.java
+++ src/main/java/com/bridgelabz/parkinglot/service/ParkingLot.java
@@ -4,6 +4,7 @@ import com.bridgelabz.parkinglot.model.Driver;
 
 public class ParkingLot {
 
+    public ParkingLot parkingLotStore;
     public Integer slotId;
     public Driver driver;
     public ParkingLot parkingLot;
@@ -13,8 +14,31 @@ public class ParkingLot {
         this.slotId = slotId;
     }
 
+    public ParkingLot(ParkingLot parkingLots) {
+        this.parkingLotStore = parkingLots;
+    }
+
+    public ParkingLot() {
+
+    }
+
     public Boolean isPark(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
         return parkingLot.driver.car.carId != null;
+    }
+
+    public ParkingLot[] fillLot(ParkingLot... parking) {
+        ParkingLot[] lotSpace = new ParkingLot[3];
+        lotSpace[0] = new ParkingLot(parking[0]);
+        lotSpace[1] = new ParkingLot(parking[1]);
+        lotSpace[2] = new ParkingLot(parking[2]);
+        lotSpace[3] = new ParkingLot(parking[3]);
+        return lotSpace;
+    }
+
+    public boolean checkLot(ParkingLot[] lotSpace) {
+        if(lotSpace.length == 4)
+            return true;
+        return false;
     }
 }
