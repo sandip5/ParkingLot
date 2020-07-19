736f3b4366b4349812dce469c97ab651eb3071ca
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-19 19:21:24 +0530

    ADDED- Test Case For Is Park When Entry Zero

--- src/test/java/com/bridgelabz/parkinglot/test/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglot/test/ParkingLotTest.java
@@ -172,4 +172,15 @@ public class ParkingLotTest {
             System.out.println(e.getMessage());
         }
     }
+
+    @Test
+    public void givenParkingLotSystem_WhenIsParkEnteredZero_ShouldThrowException() {
+        try {
+            parkingLotSystem.park(1001, 101);
+            boolean parkingStatus = parkingLotSystem.isPark(101);
+        } catch (ParkingLotException e){
+            Assert.assertEquals(ParkingLotException.ExceptionType.NULL_VALUE, e.type);
+            System.out.println(e.getMessage());
+        }
+    }
 }
