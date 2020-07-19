397933ce169a2c3f208e60ee8860c4f7cb39c887
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-19 19:02:36 +0530

    ADDED- Test Case For Zero Entry In Vehicle

--- src/test/java/com/bridgelabz/parkinglot/test/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglot/test/ParkingLotTest.java
@@ -113,9 +113,22 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenParkingLotSystem_WhenTicketNumberZero_ShouldThrowException() throws ParkingLotException {
-        parkingLotSystem.park(0, 101);
-        boolean parkingStatus = parkingLotSystem.isPark(101);
-        Assert.assertTrue(parkingStatus);
+    public void givenParkingLotSystem_WhenTicketNumberZero_ShouldThrowException() {
+        try {
+            parkingLotSystem.park(0, 101);
+        }catch (ParkingLotException e){
+            Assert.assertEquals(ParkingLotException.ExceptionType.ZERO_VALUE, e.type);
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenParkingLotSystem_WhenVehicleHaveZero_ShouldThrowException() {
+        try {
+            parkingLotSystem.park(10, 0);
+        }catch (ParkingLotException e){
+            Assert.assertEquals(ParkingLotException.ExceptionType.ZERO_VALUE, e.type);
+            System.out.println(e.getMessage());
+        }
     }
 }
